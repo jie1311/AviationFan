2ab851431da9975a49aa7f2e790777c56fcaee8e
jie1311 <tommy911113@gmail.com>
2017-02-18 14:33:42 +1100

    UI improvement
    UI improvement

--- src/main/java/application/configs/WebSecureConfig.java
+++ src/main/java/application/configs/WebSecureConfig.java
@@ -24,7 +24,7 @@ public class WebSecureConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers( "/", "/getAircraft", "/getAirport", "/getRoute", "/css/*", "/img/*").permitAll()
+                .antMatchers(  "/getAircraft", "/getAirport", "/getRoute", "/css/*", "/img/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
